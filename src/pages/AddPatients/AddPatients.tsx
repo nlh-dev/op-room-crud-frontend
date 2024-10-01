3b41d09e2dbdbb68cf1e834f86d02c93ba9f07ef
Eduardo <eduardokunmixyeah@gmail.com>
2024-10-01 19:58:27 -0400

    add paginator of table and change edit data in patient prev

--- src/pages/AddPatients/AddPatients.tsx
+++ src/pages/AddPatients/AddPatients.tsx
@@ -219,9 +219,6 @@ export const AddPatients = () => {
                         locale={es}
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
                         initialFocus
                       />
                     </PopoverContent>
